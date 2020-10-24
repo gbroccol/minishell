36dfd6deafa8b0bfa20a5397f5f07788bc03cb13
Anastasiq Pchelina <gbroccol@mi-i1.msk.21-school.ru>
2020-10-24 16:46:49 +0300

    libft

--- libft/libft.h
+++ libft/libft.h
@@ -58,6 +58,7 @@ char				*ft_strjoin(char const *s1, char const *s2);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
 size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 int					ft_strlen(const char *str);
+int					ft_strlen_n(const char *str, char smb);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strnstr(const char *big, const char *little,\
@@ -67,5 +68,6 @@ char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+int					get_next_line(int fd, char **line);
 
 #endif
--- minishell.h
+++ minishell.h
@@ -1,9 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "errors.h"
+# include "parsing.h"
+
+
 # include <unistd.h> // write, open, read, close, chdir, fork, exec - ???, pid_t - ???
 # include <stdlib.h> // malloc, free, realloc - NO, exit, execvp - NO, EXIT_SUCCESS, EXIT_FAILURE
-# include <sys/types.h> // , wait
+# include <sys/types.h> // wait
     // fork - 
     // wait -
 #include <sys/wait.h> // waitpid() и связанные макросы
@@ -18,22 +22,24 @@
 // , stat, lstat, fstat, execve, dup, dup2, pipe,
 // opendir, readdir, closedir, strerror, errno
 
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-**  read_line
+**  commands
 */
-char				*lsh_read_line(void);
+# define TYPE_CD 1
+# define TYPE_PWD 2
+# define TYPE_ECHO 3
+# define TYPE_EXIT 4
+# define TYPE_EXPORT 5
+# define TYPE_ENV 6
+# define TYPE_UNSET 7
+# define TYPE_BIN 8
+# define TYPE_PIPE 9
+# define TYPE_RD_DB_OUT 10
+# define TYPE_RD_S_OUT 11
+# define TYPE_RD_INPUT 12
+
+
+
 
 /*
 **  split_line
