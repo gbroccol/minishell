f466050d3fa0fb6937fd0c345145e6b60b0248d3
Anastasiq Pchelina <gbroccol@mi-i5.msk.21-school.ru>
2020-11-20 17:00:01 +0300

    CD EXPORT UNSET

--- parsing/command.c
+++ parsing/command.c
@@ -6,7 +6,7 @@
 /*   By: gbroccol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 19:31:56 by gbroccol          #+#    #+#             */
-/*   Updated: 2020/11/18 18:05:03 by gbroccol         ###   ########.fr       */
+/*   Updated: 2020/11/20 15:31:04 by gbroccol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,18 @@ int				command(t_token *tok)
 		if (!(tok->cmd = ft_strdup(tok->args[0])))
 			return (-1);
 		index = 0;
-		if (tok->args[0][0] == '=' ||
-				ft_isdigit(tok->args[0][0]) ||
-				check_env_key(tok->args[0]) ||
-				!ft_strchr(tok->args[0], '='))
+		if (tok->args[0][0] == '=' || ft_isdigit(tok->args[0][0]) ||
+				check_env_key(tok->args[0]) || !ft_strchr(tok->args[0], '='))
 		{
-			while (tok->args[0][index] != '\0')
+			if (ft_strcmp(tok->args[0], "CD") != 0 &&
+					ft_strcmp(tok->args[0], "EXPORT") != 0 &&
+					ft_strcmp(tok->args[0], "UNSET") != 0)
 			{
-				tok->args[0][index] = ft_tolower(tok->args[0][index]);
-				index++;
+				while (tok->args[0][index] != '\0')
+				{
+					tok->args[0][index] = ft_tolower(tok->args[0][index]);
+					index++;
+				}
 			}
 		}
 		choose_com(tok, tok->args[0]);
